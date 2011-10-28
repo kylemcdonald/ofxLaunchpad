818c9e610a4a0cfe1b42bed5163976571a358a98
Kyle McDonald <kyle@kylemcdonald.net>
2011-10-28 15:07:24 +0900

    adding more from the spec, adding notes

--- src/ofxLaunchpad.cpp
+++ src/ofxLaunchpad.cpp
@@ -3,16 +3,20 @@
 // if you click off the window, there will be too many messages
 // and the midi will fill up and have trouble
 // need some kind of rate limiting buffer?
+
+void ofxLaunchpad::setup(int port) {
+	midiIn.openPort(port);
+	midiOut.openPort(port);
+	setAll(OFF_BRIGHTNESS_MODE);
+	setMappingMode(XY_MAPPING_MODE);
+}
+
 void ofxLaunchpad::setLed(int key, int velocity) {
 	midiOut.sendNoteOn(1, key, velocity);
 }
 
-void ofxLaunchpad::reset() {
-	midiOut.sendControlChange(1, 0, 0);
-}
-
 void ofxLaunchpad::setMappingMode(MappingMode mappingMode) {
-	midiOut.sendControlChange(1, 0, mappingMode == LAUNCHPAD_XY_MODE ? 1 : 2);
+	midiOut.sendControlChange(1, 0, mappingMode == XY_MAPPING_MODE ? 1 : 2);
 }
 
 
@@ -50,21 +54,26 @@ void ofxLaunchpad::set(ofPixels& pix, bool clear, bool copy) {
 	midiOut.sendNoteOn(1, 127, 0);
 }
 
-void ofxLaunchpad::setAll(int red, int green, bool clear, bool copy) {
-	int colorMask = 0x03;
-	int velocity =
-	((green & colorMask) << 4) |
-	((clear ? 1 : 0) << 3) |
-	((copy ? 1 : 0) << 2) |
-	((red & colorMask) << 0);
-	for(int i = 0; i < 40; i++) {
-		midiOut.sendNoteOn(3, velocity, velocity);
-	}
+void ofxLaunchpad::setBufferingMode(bool copy, bool flash, int update, int display) {
+	int bufferMask = 0x01;
+	int bufferingMode =
+		(0 << 6) |
+		(1 << 5) |
+		((copy ? 1 : 0) << 4) |
+		((flash ? 1 : 0) << 3) |
+		((update & bufferMask) << 2) |
+		(0 << 1) |
+		((display & bufferMask) << 0);
+	midiOut.sendControlChange(1, 0, bufferingMode);
 }
 
-void ofxLaunchpad::setup(int port) {
-	midiIn.openPort(port);
-	midiOut.openPort(port);
-	reset();
-	setMappingMode(LAUNCHPAD_XY_MODE);
+void ofxLaunchpad::setAll(BrightnessMode brightnessMode) {
+	int mode;
+	switch(brightnessMode) {
+		case OFF_BRIGHTNESS_MODE: mode = 0; break;
+		case LOW_BRIGHTNESS_MODE: mode = 125; break;
+		case MEDIUM_BRIGHTNESS_MODE: mode = 126; break;
+		case FULL_BRIGHTNESS_MODE: mode = 127; break;
+	}
+	midiOut.sendControlChange(1, 0, mode);
 }
--- src/ofxLaunchpad.h
+++ src/ofxLaunchpad.h
@@ -1,3 +1,12 @@
+/*
+ this is a fairly straightforward implementation of the spec for the novation
+ launchpad, as described in the programmer's guide. the 'low level methods' are
+ taken almost directly from the documentation.
+ 
+ setAll() handles the LED test mode, and also incorporates the reset function
+ with OFF_BRIGHTNESS_MODE.
+*/
+
 #pragma once
 
 #include "ofMain.h"
@@ -5,16 +14,21 @@
 
 class ofxLaunchpad {
 public:	
-	enum MappingMode {LAUNCHPAD_XY_MODE, LAUNCHPAD_DRUM_MODE};
+	enum MappingMode {XY_MAPPING_MODE, DRUM_MAPPING_MODE};
+	enum BrightnessMode {OFF_BRIGHTNESS_MODE, LOW_BRIGHTNESS_MODE, MEDIUM_BRIGHTNESS_MODE, FULL_BRIGHTNESS_MODE};
 	
 	void setup(int port);
 	
+	// high level methods
 	void setLed(int key, int velocity);
+	void set(ofPixels& pix, bool clear = true, bool copy = true);	
+	
+	// low level methods
+	void setLed(int row, int col, int red, int green, bool clear = true, bool copy = true);
 	void reset();
 	void setMappingMode(MappingMode mappingMode);
-	void setLed(int row, int col, int red, int green, bool clear = true, bool copy = true);
-	void setAll(int red, int green, bool clear = true, bool copy = true);
-	void set(ofPixels& pix, bool clear = true, bool copy = true);	
+	void setBufferingMode(bool copy, bool flash, int update, int display); // also resets the flash timer
+	void setAll(BrightnessMode brightnessMode = OFF_BRIGHTNESS_MODE); // resets all other data
 	
 protected:
 	ofxMidiOut midiOut;
